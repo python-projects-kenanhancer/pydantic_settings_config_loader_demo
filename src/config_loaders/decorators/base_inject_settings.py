803fce69708de3c5d862d5fddf24c7a166f9e531
kenan hancer <kenanhancer@hotmail.com>
2024-12-23 04:31:11 +0000

    inject_settings decorator is refactored for settings already passed from unit test

--- src/config_loaders/decorators/base_inject_settings.py
+++ src/config_loaders/decorators/base_inject_settings.py
@@ -15,13 +15,20 @@ def inject_settings(
     loader_func: Callable[..., TSettings], SettingsClass: Type[TSettings], **loader_args: Any
 ) -> Callable[[Callable[..., Any]], Callable[..., Any]]:
     def decorator(func: Callable[..., Any]) -> Callable[..., Any]:
+        # We store the loaded settings as an attribute on the function object
         func._settings = None
 
         def wrapper(*args: Any, **kwargs: Any) -> Any:
-            if func._settings is None:
-                settings_instance = loader_func(SettingsClass=SettingsClass, **loader_args)
-                func._settings = settings_instance
-            return func(*args, **kwargs, settings=func._settings)
+            # If 'settings' wasn't provided by the caller, inject it
+            if "settings" not in kwargs:
+                if func._settings is None:
+                    settings_instance = loader_func(SettingsClass=SettingsClass, **loader_args)
+                    func._settings = settings_instance
+                kwargs["settings"] = func._settings
+
+            # Now call the original function,
+            # passing either the user-supplied settings or our loaded one.
+            return func(*args, **kwargs)
 
         return wrapper
 
